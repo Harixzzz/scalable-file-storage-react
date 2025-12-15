5ff4202d6c0030d9f32d87e3cb1f2dd4e2f9c096
Harish <harixzzz@gmail.com>
2025-12-15 11:20:09 -0500

    Fix Cognito domain typo (case-sensitive)

--- scalable-file-storage-app-main/frontend/src/App.js
+++ scalable-file-storage-app-main/frontend/src/App.js
@@ -1,47 +1,7 @@
-// src/App.js
-import { useEffect } from "react";
-
 function App() {
-  useEffect(() => {
-    const params = new URLSearchParams(window.location.search);
-    const code = params.get("code");
-
-    if (code) {
-      exchangeCodeForToken(code);
-    }
-  }, []);
-
-  const exchangeCodeForToken = async (code) => {
-    const body = new URLSearchParams({
-      grant_type: "authorization_code",
-      client_id: "120vfbb5r97d01vio63cisbqti",
-      code: code,
-      redirect_uri: "http://localhost:3000/",
-    });
-
-    const response = await fetch(
-      "https://us-east-1-0Q99e553N.auth.us-east-1.amazoncognito.com/oauth2/token",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/x-www-form-urlencoded",
-        },
-        body,
-      }
-    );
-
-    const data = await response.json();
-    console.log("TOKENS:", data);
-
-    localStorage.setItem("id_token", data.id_token);
-    localStorage.setItem("access_token", data.access_token);
-
-    window.history.replaceState({}, document.title, "/");
-  };
-
   const login = () => {
     window.location.href =
-      "https://us-east-1-0Q99e553N.auth.us-east-1.amazoncognito.com/login" +
+      "https://us-east-1-0o99e553n.auth.us-east-1.amazoncognito.com/login" +
       "?client_id=120vfbb5r97d01vio63cisbqti" +
       "&response_type=code" +
       "&scope=openid+email+phone" +
@@ -49,7 +9,7 @@ function App() {
   };
 
   return (
-    <div style={{ padding: "40px" }}>
+    <div>
       <button onClick={login}>Login</button>
     </div>
   );
